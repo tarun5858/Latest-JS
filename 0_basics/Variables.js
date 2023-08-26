6494cd3184fb6d3466c8d63ddbd2b73d336e6778
Tarun Singh Khangarot <114020453+tarun5858@users.noreply.github.com>
2023-08-26 12:47:25 +0000

    understanding of variables

---
+++ 0_basics/Variables.js
@@ -0,0 +1,9 @@
+const userId = 244556;
+let userMail= "Tarun@123";
+var userPassword = "123456";
+userCity = "Jaipur";
+let userState;
+
+
+// userId = 2;
+console.table([userId,userMail,userPassword,userCity,userState])
