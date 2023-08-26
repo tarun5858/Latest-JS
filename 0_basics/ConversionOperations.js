4943498f9904484d9fd011bd8b82e9bf8f060c8a
Tarun Singh Khangarot <114020453+tarun5858@users.noreply.github.com>
2023-08-26 13:37:04 +0000

    conversion of data types

---
+++ 0_basics/ConversionOperations.js
@@ -0,0 +1,33 @@
+// Data types 
+
+// (non premitive)
+// string => "Tarun";
+// number => 3;
+// bigint => 3n;
+// boolean => true/false;
+// symbol => for giving unique identification of data types;
+// null => object 
+// undefined => value is not defined;
+
+
+let score = "";
+// let changeVal = Number(userName);
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Boolean(score);
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0;
+// " " => false; "abc" => true;
+
+
+let someNum = 33;
+let changeValue = String(someNum);
+console.log(changeValue);
+console.log(typeof changeValue);
